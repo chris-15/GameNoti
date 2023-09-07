61137473e485db6dd16fb75d780d49b704c39534
Chris <christopher.sarm15@gmail.com>
2023-09-07 18:49:13 -0400

    started to create a main page

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,11 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { StatusBar } from "expo-status-bar";
+import { Icon } from "@rneui/themed";
 
-const MainScreen = () => {
+const MainScreen = ( {navigation} ) => {
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: 'GameNoti',
+      headerLeft: () => <View style={{ marginLeft: 20 }}>
+        <Text className="text-white">LEFT</Text>
+      </View>,
+      headerRight: () => <View style={{ marginRight: 20}}>
+        <Icon name="search" color="white" />
+      </View>
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>I am the main screen</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-neutral-800 justify-center items-center">
+      <StatusBar style="light" />
+      <View >
+        <Text className="text-white">This is the main page- send noti to your friends!</Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
